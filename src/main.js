a9a7f30ec46951253a8b4e6ffb8d3770b1603800
MGabrielaVillon <117649344+MGabrielaVillon@users.noreply.github.com>
2026-06-18 19:30:15 +0000

    nombre y matricula completo

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import './style.css'
 document.querySelector('#app').innerHTML = `
   <div class="max-w-6xl mx-auto p-6">
 
-    
+    <h2 class="text-2xl font-bold mb-4">
+      Matrícula: MARIA GABRIELA VILLON PALMA - 202105780
+    </h2>
 
     <!-- DATATABLE -->
     <div class="relative overflow-x-auto shadow-md sm:rounded-lg">
